06819006db226f2372903dae47680030ef2ac0d4
gabi2002 <32403861+gabi2002@users.noreply.github.com>
2018-03-08 13:52:48 +0200

    flashlight c# unity script

---
+++ uscripts/FlashLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour {
+	public Light flashLight;
+	private bool isActive;
+	private bool isNotActive;
+
+	// Use this for initialization
+	void Start () {
+		isActive = true;
+		isNotActive = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if (isActive == false) {
+				flashLight.enabled = true;
+				isActive = true;
+			} 
+			else if (isActive == true) 
+			{
+				flashLight.enabled = false;
+				isActive = false;
+			}
+
+			else if (isNotActive == true) {
+				flashLight.enabled = false;
+				isNotActive = false;
+			} 
+			else if (isNotActive == false) 
+			{
+				flashLight.enabled = true;
+				isNotActive = true;
+			}
+		}
+		
+	}
+}
